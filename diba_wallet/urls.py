73abc73d505e21d9d75f577858836cc423ce5741
Mario Pleitez <mariopleitez@gmail.com>
2024-11-01 07:56:20 -0600

    asdfasdfasdfsdaf

--- diba_wallet/urls.py
+++ diba_wallet/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def working(request):
+    return HttpResponse("Up and Running!")
 
 urlpatterns = [
+    path("", working),
     path('admin/', admin.site.urls),
     path('api/', include('wallet.urls')),
 ]
+
+
